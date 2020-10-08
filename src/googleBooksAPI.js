36d2d1df0a64bb086c5c0a1e99a646938f5b2ce5
bmarcosps <brunomarcos@ice.ufjf.br>
2020-10-08 17:59:56 -0300

    Add simple code documentation

--- src/googleBooksAPI.js
+++ src/googleBooksAPI.js
@@ -1,14 +1,17 @@
 const axios = require('axios').default;
 
-
+/**
+ * 
+ * @param {string} isbn: isbn do livro a ser procurado
+ * @return {promise} retorno do google com todas as informações disponíveis na api.
+ *                      Os resultados são tratados no local em que a callback é chamada
+ * 
+ */
 const fetchBookGoogle = async (isbn) => {
-
     return axios.get('https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn)
     .then(function (response) {
         return response.data
     })
-
-
 }
 
 export default fetchBookGoogle;
