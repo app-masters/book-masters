1dc9d4dcd265545758a9c51eb1f18f80d818771a
GabrielCarmoSilva <gabriel.ccarmo@gmail.com>
2020-10-08 11:15:27 -0300

    Página sobre

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import Product from "./Products";
+import About from "./About";
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/products/:id" component={Product} />
+      <Route path="/about" component={About} />
     </Switch>
   </BrowserRouter>
 );
