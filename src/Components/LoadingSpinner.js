eb717723d3b62425d624bb7e672fbd858c8c26d4
bmarcosps <brunomarcos@ice.ufjf.br>
2020-10-13 10:42:11 -0300

    Add Loading spinner, Add Fetch to get books from server

---
+++ src/Components/LoadingSpinner.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactLoading from "react-loading";
+
+const LoadingSpinner = () => {
+    return (
+        <ReactLoading type="spin" color="#0ab6ff" />
+    )
+    
+};
+
+export default LoadingSpinner;
