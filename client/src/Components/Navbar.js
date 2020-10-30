1107fe34c74cfd8ef2eb688c497c9deb61ca0950
Lavínia Beghini <lavinia.beghini@gmail.com>
2020-10-30 11:43:11 -0300

    Update Navbar.js

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,10 +1,10 @@
-
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import { Button } from "@material-ui/core";
 import { Link as RouterLink } from 'react-router-dom';
 import { navbar } from "../assets/css/makeStyles"
+import {Container} from '@material-ui/core';
 
 
 const Navbar = () => {
