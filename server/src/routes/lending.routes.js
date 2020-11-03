81efe15fefe119370ecacaf8d7d7ace3d576022c
bmarcosps <brunomarcos@ice.ufjf.br>
2020-11-03 10:27:24 -0300

    Add reserve and return endpoints

--- server/src/routes/lending.routes.js
+++ server/src/routes/lending.routes.js
@@ -6,11 +6,13 @@ const lendingRouter = Router()
 
 lendingRouter.get("/lendings/", LendingController.getAll)
 lendingRouter.get("/lendings/book/:id", LendingController.getAllBookLending)
-lendingRouter.get("/lendings/reserve", LendingController.reserve)
 lendingRouter.get("/lendings/:id", LendingController.getById)
 lendingRouter.put("/lendings/:id", LendingController.update)
 lendingRouter.delete("/lendings/:id", LendingController.delete)
-lendingRouter.post("/lending/", LendingController.lending)
+lendingRouter.post("/lendings/", LendingController.lending)
+lendingRouter.post("/lendings/reserve", LendingController.reserve)
+lendingRouter.post("/lendings/return", LendingController.returnBook)
+
 
 
 
