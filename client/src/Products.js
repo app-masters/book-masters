5e3370db749e5957ae374bccf9e31f124d87473d
Lavínia Beghini <lavinia.beghini@gmail.com>
2020-11-04 09:52:38 -0300

    Update Products.js

--- client/src/Products.js
+++ client/src/Products.js
@@ -43,7 +43,7 @@ export const Product = (props) => {
 			}
 			const json = response.data;
 			console.log(json)
-			setLendings(json)
+			setLendings(json.lendings)
 		}catch(error){
 			console.log(error)
 		}
