685e6aeacf58201e2afd17113e3ee71d1e1fbd30
Bruno Pinheiro <brunomarcospsilva@gmail.com>
2020-12-18 11:05:00 -0300

    Fix files

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express"
 import userController from "../controllers/userController"
 
-const userRouter = Router()
+const userRouter = Router() 
 
 
 userRouter.get("/users/", userController.getAll)
