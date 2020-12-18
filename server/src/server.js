c3eed772824218adbc3024878915c0eec51cce92
Bruno Pinheiro <brunomarcospsilva@gmail.com>
2020-12-18 11:26:57 -0300

    Change Port variable

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,7 @@ import app from "./app.js"
 
 import "dotenv/config.js"
 
-const port = process.env.APP_PORT || 3333
+const port = process.env.PORT || 3333
 
 app.listen(port, () => {
   console.log(`Servidor iniciado na porta ${port}!`)
