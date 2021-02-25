a786caa20ffb78f46cacd6339cf63cf630c0d775
Vini98br <viniciuss10@hotmail.com>
2021-02-25 17:06:47 -0300

    refactoring routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import routes from './routes/index.js';
+import routes from './routes';
 
 import 'dotenv/config.js';
 import './database/connection.js';
--- server/src/routes/notify.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import NotifyAvailabilityController from '../controllers/NotifyAvailabilityController.js';
-import authMiddleware from '../middleware/auth.js';
-
-const notifyRouter = express.Router();
-
-notifyRouter.get('/notifyMe/:bookId', authMiddleware, NotifyAvailabilityController.notifyMe);
-
-export default notifyRouter;
